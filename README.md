2cd4290438b4132dcd3d4c0429efd508779cab89
vitaliiIT <viza.uz.work@gmail.com>
2024-12-26 18:10:57 +0200

    add brain-gcd

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ### my project brain-games
 [![Maintainability](https://api.codeclimate.com/v1/badges/<badge-id>/maintainability)](https://codeclimate.com/github/<user>/<repo>/maintainability)
 
-### recording game
+### recording game even
 [![asciicast](https://asciinema.org/a/590UtX6yhMaPAhLDUg7RlBWiB.svg)](https://asciinema.org/a/590UtX6yhMaPAhLDUg7RlBWiB)
 
+### recording game gcd
+[![asciicast](https://asciinema.org/a/gOo07GHNNHydhoDA2jznP24Bn.svg)](https://asciinema.org/a/gOo07GHNNHydhoDA2jznP24Bn)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,25 @@
+import random
+from math import gcd
+from brain_games.cli import welcome_user
+
+def main():
+    user_name = welcome_user()
+    print("Find the greatest common divisor of given numbers.")
+
+    rounds_to_win = 3
+    for _ in range(rounds_to_win):
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 100)
+        print(f"Question: {number1} {number2}")
+        
+        correct_answer = gcd(number1, number2)
+        user_answer = input("Your answer: ").strip()
+
+        if not user_answer.isdigit() or int(user_answer) != correct_answer:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {user_name}!")
+            return
+
+        print("Correct!")
+
+    print(f"Congratulations, {user_name}!")
